b25a154dfc4296ef4dd63c1588bbfb758b43cdf1
Weil_燃燒那不大的大腦 <74590513+f0955710119@users.noreply.github.com>
2022-02-04 23:22:39 +0800

    Runtime: 160 ms, faster than 97.23% of JavaScript online submissions & Memory Usage: 50.9 MB, less than 14.82%

--- 09-palindrome-number/index.js
+++ 09-palindrome-number/index.js
@@ -6,15 +6,27 @@ var isPalindrome = function (x) {
   //     0)確定是基數還偶數長度的數值
   //     1)利用長度去剖半作對比
   //     2)最後加的值不可以大於長度減1
+  var number = x;
   if (!Number.isInteger(+x)) {
     console.log('Please give an INT');
-    return null;
+    return false;
   }
+
+  if (number < 0) {
+    console.log('Please give an positive INT');
+    return false;
+  }
+
   var result;
   var i = 1;
-  var numStr = '' + x;
+  var numStr = '' + number;
   var middle = Math.trunc(numStr.length / 2);
   var middleFixed = Math.trunc(numStr.length / 2);
+
+  if (middle === 0) {
+    result = true;
+    return result;
+  }
 
   //      基數
   if (!Number.isInteger(numStr.length / 2)) {
@@ -49,4 +61,4 @@ var isPalindrome = function (x) {
   return result;
 };
 
-console.log(isPalindrome('121s'));
+console.log(isPalindrome(-121));
