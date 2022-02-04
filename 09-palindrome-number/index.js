083e16c03dd53573c72997b7f480ad7a1ff2d569
Weil_燃燒那不大的大腦 <74590513+f0955710119@users.noreply.github.com>
2022-02-04 23:09:28 +0800

    complete palindrome number but without ckecking whether the input number is negative and with checking string

---
+++ 09-palindrome-number/index.js
@@ -0,0 +1,52 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function (x) {
+  //     0)確定是基數還偶數長度的數值
+  //     1)利用長度去剖半作對比
+  //     2)最後加的值不可以大於長度減1
+  if (!Number.isInteger(+x)) {
+    console.log('Please give an INT');
+    return null;
+  }
+  var result;
+  var i = 1;
+  var numStr = '' + x;
+  var middle = Math.trunc(numStr.length / 2);
+  var middleFixed = Math.trunc(numStr.length / 2);
+
+  //      基數
+  if (!Number.isInteger(numStr.length / 2)) {
+    while (middle > 0) {
+      var number1 = numStr[middleFixed - i];
+      var number2 = numStr[middleFixed + i];
+
+      if (number1 !== number2) {
+        result = false;
+        break;
+      } else {
+        result = true;
+      }
+      i++;
+      middle--;
+    }
+    //     偶數
+  } else {
+    while (middle > 0) {
+      middle--;
+      var number1 = numStr[middleFixed + i - 1];
+      var number2 = numStr[middle];
+      if (number1 !== number2) {
+        result = false;
+        break;
+      } else {
+        result = true;
+      }
+      i++;
+    }
+  }
+  return result;
+};
+
+console.log(isPalindrome('121s'));
